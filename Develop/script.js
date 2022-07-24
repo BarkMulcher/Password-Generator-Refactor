7a216ac86c57a6db8a0b777805b4d0dafb6014c5
BarkMulcher <lucas.c.haskell@gmail.com>
2022-07-24 17:11:06 -0400

    JS fixes and readme updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,41 @@
 // // Assignment code here:
-
-
-// initiate password generate function:
+  
 function generatePassword() {
-
+ 
   // declare variables for sets of criteria:
   var strUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var strLowerCase = "abcdefghijklmnopqrstuvwxyz";
   var strSpecChar = "!@#$%^&*()-=";
   var strNum = "12345678990";
-  const strTotal = "";
+  var strTotal = "";
 
-  // confirm pop-ups w/ text: 
+  
+  // declare prompts w/ text: 
   // create if/else loops 
-askLength = window.prompt("Please enter the number of characters required, between 8 & 128.");
-  if (askLength < 8 || askLength > 128) {
-    window.alert("WRONG. 8 - 128 characters required to proceed.");
-  } else if (isNaN(askLength)) { 
-    return generatePassword();
-  }
-askUpperCase = window.confirm("Pop quiz, hotshot:\nDoes your password require Upper Case?\nOK for Yes, Cancel for No");
-askLowerCase = window.confirm("Pop quiz, hotshot:\nDoes your password require lower case?\nOK for Yes, Cancel for No"); 
-askSpecChar = window.confirm("Pop quiz, hotshot:\nDoes your password require  special ch@racter$?\nOK for Yes, Cancel for No");
-askNum = window.confirm("Pop quiz, hotshot:\nDoes your password require numbers?\nOK for Yes, Cancel for No");
+  var askLength = window.prompt("Please enter the number of characters required, between 8 & 128.");
+    if (askLength < 8 || askLength > 128) {
+      window.alert("WRONG. 8 - 128 characters required to proceed.");
+    } else if (isNaN(askLength)) {
+      generatePassword();
+    }
+  var askUpperCase = window.confirm("Pop quiz, hotshot:\nDoes your password require Upper Case?\nOK for Yes, Cancel for No");
+  var askLowerCase = window.confirm("Pop quiz, hotshot:\nDoes your password require lower case?\nOK for Yes, Cancel for No"); 
+  var askSpecChar = window.confirm("Pop quiz, hotshot:\nDoes your password require  special ch@racter$?\nOK for Yes, Cancel for No");
+  var askNum = window.confirm("Pop quiz, hotshot:\nDoes your password require numbers?\nOK for Yes, Cancel for No");
 
-// check for at least one criteria
-  if (askUpperCase === false && askLowerCase === false && askSpecChar === false && askNum === false);
+// check for at least one criteria:
+  if (askUpperCase != true && askLowerCase != true && askSpecChar != true && askNum != true) {
+    // notify user of need for criteria:
     window.alert("Your password must have a criterion to proceed.");
-    return generatePassword();
-}
-
+    generatePassword(); 
+  }
+ 
 // create string combinations:
   if (askUpperCase) {
     strTotal = strTotal.concat(strUpperCase);
   }
   if (askLowerCase) {
-    strTotal = strTotal.concat(strLowerCase);
+  strTotal = strTotal.concat(strLowerCase);
   }
   if (askSpecChar) {
     strTotal = strTotal.concat(strSpecChar);
@@ -44,63 +44,28 @@ askNum = window.confirm("Pop quiz, hotshot:\nDoes your password require numbers?
     strTotal = strTotal.concat(strNum);
   }
 
-  for (let i = 0; i < 128; i++) {
-    var char = Math.floor(Math.random()
-                  * strTotal.length + 1);
-    
-    password += strTotal.charAt(char)
-    password = "AB" + string
+
+  var password = "";       
+
+  for (let i = 0; i < askLength; i++) {
+    var pw = [Math.floor(Math.random() * strTotal.length)]; 
+    password += strTotal.charAt(pw);
   }
-
   return password;
-
-
-function pwGen() {
-  pwrd.innerHTML = generatePassword();
-
 }
 
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
   // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
-// function generatePassword() {
-
-//   var password = "";
-//   var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=" + // not this
-//                 "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_="; // not this 
-//  var strUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//  var strLowerCase = "abcdefghijklmnopqrstuvwxyz";
-//  var strSpecChar = "!@#$%^&*()-=";
-//  var strNum = "12345678990";
-//  var strTotal = []; ?
-
-
-
-
-
-
-
-var pwrd = document.getElementById("gen");
-
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-/* <button id=**"generate"** class="btn">Generate Password</button> */
-
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-// var generate = document.getElementById("generate");
 
 // eventListener for Generate Button:
 generateBtn.addEventListener("click", writePassword);
+// initiate password generate function:
--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Our assignment was to take the current non-functional password generation page a
 
 Learning more about prompt(), confirm(), and alert() methods was paramount. I learned the distinction in their purposes, and had to decide the best fit for the context. Prompt() required input and room for user error, so I thought it best to limit its use. I chose confirm() for the majority of criteria prompts, then ended up using all three methods in my code. I also *really* wanted the user to be able to end the process by clicking away from these pop-ups. Ultimately, I could not find a way to do this in the assignment's time frame, but I wish to incorporate this functionality in the future.
 
-Many examples of password generators utilized arrays, but I am trying to use a string for each possible set of criteria.
+As with all past assignments, I spent an obscene amount of time debugging a very simple syntax issue. And when I thought I had it completed, I realized that if I ran the generator more than a few times, it would occasionally not include special characters. I fixed this by removing a "+1" from the Math object calculation. I'm not entirely sure why that fixed the issue, but I will research this.
+
+
+
+
 
 
 
